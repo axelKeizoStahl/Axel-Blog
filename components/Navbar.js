b0b3cddcc6b0d57df18a6ebbfcf9f8b4031c3232
axelKeizoStahl <axelkeizo@gmail.com>
2024-02-25 23:28:51 -0500

    initial commit

---
+++ components/Navbar.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <Link href={"/"}>Home</Link>
+      <Link href={"/addPost"}>NewPost</Link>
+    </nav>
+  );
+}
