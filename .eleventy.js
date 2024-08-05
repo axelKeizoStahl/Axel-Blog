022179b66dfed7560c418bd90030cdc9e02e3b23
axelKeizoStahl <axelkeizo@gmail.com>
2024-08-04 22:21:35 -0400

    workflow

--- .eleventy.js
+++ .eleventy.js
@@ -58,7 +58,7 @@ module.exports = function (eleventyConfig) {
 			input: "content", // default: "."
 			includes: "../_includes", // default: "_includes"
 			data: "../_data", // default: "_data"
-			output: "docs",
+			output: "_site",
 		},
 	};
 };
